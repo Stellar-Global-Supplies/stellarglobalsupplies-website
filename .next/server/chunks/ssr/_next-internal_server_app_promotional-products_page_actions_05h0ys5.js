f72bcc7d45b3182f154552b5475be9fdc20d1ca4
Prasad Bhavsar <63907398+Prasadpb77@users.noreply.github.com>
2026-08-11 04:52:07 +0000

    fix: untrack stale build artifacts from git

--- .next/server/chunks/ssr/_next-internal_server_app_promotional-products_page_actions_05h0ys5.js
+++
@@ -1,3 +0,0 @@
-module.exports=[36281,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_promotional-products_page_actions_05h0ys5.js.map
--- .next/static/chunks/0eaz3qf5-q__w.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,72430,t=>{"use strict";var e=t.i(43476);t.s(["default",0,function({data:t}){return(0,e.jsx)("script",{type:"application/ld+json",dangerouslySetInnerHTML:{__html:JSON.stringify(t)}})}])}]);
